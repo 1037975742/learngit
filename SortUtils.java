748a4475d58ac6dd48aff85178e02dcf597dd3e4
wangdenghui <wangdenghui351@pingan.com.cn>
2016-05-15 16:53:02 +0800

    添加新文件到sort

---
+++ SortUtils.java
@@ -0,0 +1,25 @@
+这是一个java开发文件
+
+
+
+public class Sort{
+
+public void main(){
+
+}
+
+ArrayList<Object> list = new ArrayList<>();
+
+public void sortByFor(){
+
+for(Object object : Object){
+
+if(object.hashCode() == 0){
+
+System.out.println("中国好");
+
+}
+
+}
+
+}
